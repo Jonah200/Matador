4d8ff5ede50168bfbc50564a2efa91d71a9c0b23
Reymundo Guerrero <rxg636@case.edu>
2026-03-05 09:52:36 -0500

    Added middleware for backend-frontend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,18 @@ import asyncio
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routers import analysis
+from fastapi.middleware.cors import CORSMiddleware  
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(analysis.router)
 
